08af80ffcb54cbb79da98aa74133de4b45e182d3
Mohamed Nabeel <137497525+nabeelmohamed@users.noreply.github.com>
2024-11-13 16:38:23 +0530

    Update iam-instance-profile.tf

--- terraform/iam-instance-profile.tf
+++ terraform/iam-instance-profile.tf
@@ -1,4 +1,4 @@
-resource "aws_iam_instance_profile" "instance-profile" {
-  name = "Jenkins-instance-profile"
-  role = aws_iam_role.iam-role.name
+resource "aws_iam_instance_profile" "jump_instance_profile" {
+  name = "jump-server-instance-profile"
+  role = aws_iam_role.jump_server_role.name
 }
