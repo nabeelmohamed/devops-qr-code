cdee7d74f18ed1df7ed16a0f7ed0ca26c113233d
Mohamed Nabeel <137497525+nabeelmohamed@users.noreply.github.com>
2024-11-13 16:46:22 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,3 +6,9 @@ variable "sg-name" {}
 variable "instance-name" {}
 variable "key-name" {}
 variable "iam-role" {}
+variable "region" {}
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+}
+variable "cluster_name" {}
